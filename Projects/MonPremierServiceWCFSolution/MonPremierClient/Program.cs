157b480175162867844084c8139d46d9ee76550c
estelle.mei <estellem148@gmail.com>
2018-12-07 14:24:34 +0100

    test

---
+++ Projects/MonPremierServiceWCFSolution/MonPremierClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonPremierClient
+{
+    class Program
+    {
+        static async void MaMethodeAsynchrone(string input, ServiceReference1.Service1Client client)
+        {
+
+            Console.WriteLine($"Prix de l'objet {input} : {await client.GetPriceAsync(input)}");
+        }
+
+        static void Main(string[] args)
+        {
+            var input = "";
+            var client = new ServiceReference1.Service1Client();
+
+            do
+            {
+                Console.WriteLine("Quel est le nom de l'objet dont vous voulez savoir le prix ?");
+
+                input = Console.ReadLine();
+
+                //Console.WriteLine($"Prix de l'objet {input} : {client.GetPrice(input)}");
+                Task.Run(() => MaMethodeAsynchrone(input, client));
+
+            } while (input != "quit");
+
+            client.Close();
+        }
+    }
+}
---
+++ Projects/MonPremierServiceWCFSolution/MonPremierServiceWCF/Service1.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MonPremierServiceWCF
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public float GetPrice(string id)
+        {
+            System.Threading.Thread.Sleep(10000);
+
+            switch (id)
+            {
+                case "chaussures":
+                    {
+                        return 49.0f;
+                    }
+                case "blouson":
+                    {
+                        return 129.99f;
+                    }
+                case "voiture":
+                    {
+                        return 32600.0f;
+                    }
+                default:
+                    {
+                        return 9999999.99f;
+                    }
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return $"Valeur passée en paramètre : {value}";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
